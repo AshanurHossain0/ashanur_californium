a25f995d525ea375d4a51f2add2fc196d2e67b54
AshanurHossain0 <ashanurhossain0@gmail.com>
2022-12-10 13:46:05 +0530

    get api assignment completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,77 +1,81 @@
 const express = require('express');
 const router = express.Router();
-const intro = require('./introduction')
-const employee = require('./employee')
-const _ = require('underscore')
-const mentorModule = require('../abc/xyz/myModule'); 
-const req = require('express/lib/request');
-const { route } = require('express/lib/application');
 
 
 router.get("/profile-details", function(req, res){
     // Write the LOGIC here
     res.send('dummy response')
 })
-
-router.get('/test-me', function (req, res) {
-    console.log("email from introduction module", intro.myEmail)
-    intro.myFunction('Sabiha')
-    console.log("email from employee module", employee.myEmail)
-
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    let result = _.first(days, 4)
-    console.log(`Result from underscore function is ${result}`)
-    console.log(`The mentor of the day is ${mentorModule.mentor}`)
-
-    res.send('any dummy text from route handler 1')
-});
-
-
-router.get('/test-me', function(req, res){
-    console.log("I am here")
-    res.send("any dummy text from route handler 2")
+router.get("/movies", function(req, res){
+    // Write the LOGIC here
+    const movies=['three idiot','krish','kal ho na ho','premer kahini','who am i?']
+    res.send(movies)
+})
+// router.get("/movies/:indexNumber", function(req, res){
+//     // Write the LOGIC here
+//     const movies=['three idiot','krish','kal ho na ho','premer kahini','who am i?']
+//     res.send(movies[req.params.indexNumber])
+// })
+router.get("/movies/:indexNumber", function(req, res){
+    // Write the LOGIC here
+    const movies=['three idiot','krish','kal ho na ho','premer kahini','who am i?']
+    if(req.params.indexNumber >=0 && req.params.indexNumber<5){
+        res.send(movies[req.params.indexNumber])
+    }
+    else{
+        res.send("invalid number, Enter a valid index!")
+    }
+})
+router.get("/films", function(req, res){
+    const film=[
+        {
+            id:1,
+            name:'three idiot'
+        },
+        {
+            id:2,
+            name:'krish',
+        },
+        {
+            id:3,
+            name:'kal ho na ho'
+        },
+        {
+            id:4,
+            name:'rang de basanti'
+        }
+    ]
+    res.send(film)
+})
+router.get("/films/:filmId", function(req, res){
+    const film=[
+        {
+            id:1,
+            name:'three idiot'
+        },
+        {
+            id:2,
+            name:'krish',
+        },
+        {
+            id:3,
+            name:'kal ho na ho'
+        },
+        {
+            id:4,
+            name:'rang de basanti'
+        }
+    ]
+    let myObj=film.find((obj)=>{
+        return (obj.id==req.params.filmId)
+    })
+    if(myObj){
+        res.send(myObj);
+    }
+    else{
+        res.send("No movie exists with this id");
+    }
 })
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
-
-// PATH Param example
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use many ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
-})
-
-// PATH Param example
-router.get("/profile/:name", function(req, res){
-    console.log('Printing the request to find out wjere name is stored',req.params)
-    console.log('user name is',req.params.name)
-    //console.log(`User requesting for profile is ${name}`)
-    res.send("dummy details")
-})
-
-// Query Param example
-router.get("/shoes", function(req, res){
-    console.log("The filter options for shoes are -",req.query)
-    //req.query.size
-    //req.query.brand
-    res.send("dummy shoes response")
-})
 
 module.exports = router;
