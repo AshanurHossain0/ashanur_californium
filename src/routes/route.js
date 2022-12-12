63958a63beec5581c3ab44dcacdd16d567fbe52f
AshanurHossain0 <ashanurhossain0@gmail.com>
2022-12-12 18:34:10 +0530

    problem1 solved

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,81 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-
-router.get("/profile-details", function(req, res){
-    // Write the LOGIC here
-    res.send('dummy response')
+router.get('/sol1',function(req,res){
+    let arr=[1,2,3,4,6,7,8,9]
+    let n=arr.length+1;
+    let maxSum=(n*(n+1))/2;
+    let minSum=arr.reduce((acc,element)=>{return acc+=element});
+    let missingNumber=(maxSum-minSum);
+    res.send({data:missingNumber})
 })
-router.get("/movies", function(req, res){
-    // Write the LOGIC here
-    const movies=['three idiot','krish','kal ho na ho','premer kahini','who am i?']
-    res.send(movies)
-})
-// router.get("/movies/:indexNumber", function(req, res){
-//     // Write the LOGIC here
-//     const movies=['three idiot','krish','kal ho na ho','premer kahini','who am i?']
-//     res.send(movies[req.params.indexNumber])
-// })
-router.get("/movies/:indexNumber", function(req, res){
-    // Write the LOGIC here
-    const movies=['three idiot','krish','kal ho na ho','premer kahini','who am i?']
-    if(req.params.indexNumber >=0 && req.params.indexNumber<5){
-        res.send(movies[req.params.indexNumber])
-    }
-    else{
-        res.send("invalid number, Enter a valid index!")
-    }
-})
-router.get("/films", function(req, res){
-    const film=[
-        {
-            id:1,
-            name:'three idiot'
-        },
-        {
-            id:2,
-            name:'krish',
-        },
-        {
-            id:3,
-            name:'kal ho na ho'
-        },
-        {
-            id:4,
-            name:'rang de basanti'
-        }
-    ]
-    res.send(film)
-})
-router.get("/films/:filmId", function(req, res){
-    const film=[
-        {
-            id:1,
-            name:'three idiot'
-        },
-        {
-            id:2,
-            name:'krish',
-        },
-        {
-            id:3,
-            name:'kal ho na ho'
-        },
-        {
-            id:4,
-            name:'rang de basanti'
-        }
-    ]
-    let myObj=film.find((obj)=>{
-        return (obj.id==req.params.filmId)
-    })
-    if(myObj){
-        res.send(myObj);
-    }
-    else{
-        res.send("No movie exists with this id");
-    }
-})
-
 
 module.exports = router;
