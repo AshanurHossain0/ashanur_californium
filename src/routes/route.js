cd463c30f4112eda813696b95c096f250755bd94
AshanurHossain0 <ashanurhossain0@gmail.com>
2022-12-12 20:32:15 +0530

    problem2 is solved

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/sol1',function(req,res){
-    let arr=[1,2,3,4,6,7,8,9]
+router.get('/sol2',function(req,res){
+    let arr=[33, 34, 35,36,38]
     let n=arr.length+1;
-    let maxSum=(n*(n+1))/2;
-    let minSum=arr.reduce((acc,element)=>{return acc+=element});
-    let missingNumber=(maxSum-minSum);
+    let maxSum=n*(arr[0]+arr[arr.length-1])/2;
+    let minSum=arr.reduce((acc,element)=>{return acc+=element})
+    let missingNumber=maxSum-minSum;
     res.send({data:missingNumber})
 })
 
