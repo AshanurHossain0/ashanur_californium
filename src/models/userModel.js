bcfac256015acc40420b2deda144981940f30abb
AshanurHossain0 <ashanurhossain0@gmail.com>
2022-12-15 20:25:17 +0530

    mongoSession1 assignment done

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,19 +1,21 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
+    bookName: String,
+    authorName: String,
+    category: String,
+    year: Number
+    // mobile: {
+    //     type: String,
+    //     unique: true,
+    //     required: true
+    // },
+    // emailId: String,
+    // gender: {
+    //     type: String,
+    //     enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+    // },
+    // age: Number,
     // isIndian: Boolean,
     // parentsInfo: {
     //     motherName: String,
