ee88f7447a06073c6b4b9804dcf264b3479dc184
AshanurHossain0 <ashanurhossain0@gmail.com>
2022-12-18 18:53:27 +0530

    mongoDbAssignment2 is completed

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose=require('mongoose')
+const bookModel=new mongoose.Schema({
+    bookName:{type:String,required:true},
+    price:{indianPrice:String,europeanPrice:String},
+    year:{type:Number,default:2021},
+    tags:[String],
+    authorName:String,
+    totalPages:Number,
+    stockAvailable:Boolean
+},{timestamps:true})
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
-    },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
-
-
-module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
+module.exports=mongoose.model('Book',bookModel)
