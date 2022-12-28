37b264d56fd951f398b463c133c7c12f927ce438
AshanurHossain0 <ashanurhossain0@gmail.com>
2022-12-28 21:39:19 +0530

    try_catch_statusCode assignment

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,18 +1,21 @@
 const jwt = require("jsonwebtoken");
 const tokenValidationAndAuthorization= function(req,res,next){
-  
-   let token = req.headers["x-auth-token"];
-  if (!token) return res.send({ status: false, msg: "token must be present" });
   try{
-    let decodedToken =jwt.verify(token, "nurasha2000");
-    if(decodedToken.userId !=req.params.userId){
-      return res.send({status:false,msg:"you are not authorized to do the operation"})
+    let token = req.headers["x-auth-token"];
+    if (!token) return res.status(400).send({ status: false, msg: "token must be present" });
+    try{
+      let decodedToken =jwt.verify(token, "nurasha2000");
+      if(decodedToken.userId !=req.params.userId){
+        return res.status(403).send({status:false,msg:"you are not authorized to do the operation"})
+      }
+      next();
     }
-    next();
+    catch(err){
+      return res.status(400).send({msg:"invalid token"})
+    }
   }
   catch(err){
-    return res.send({ status: false, msg: "token is invalid" });
+    return res.status(500).send({status:false,msg:err.message})
   }
-  
 }
 module.exports.tokenValidationAndAuthorization=tokenValidationAndAuthorization;
