b6724372f1e5f46ca304308b5ce9f989c49a552f
19Beulah <p.beulah5002@gmail.com>
2023-01-16 18:04:34 +0530

    added

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const app=express()
+const mongoose= require("mongoose")
+app.use(express.json());
+
+
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb+srv://devendra_29:I28Cx63EjuXQjHtQ@devendra.ytysqub.mongodb.net/group17Database",{useNewUrlParser:true})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+
+app.use('/',route)
+
+
+app.listen(3000, function () {
+    console.log('Express app running on port ' +  3000)
+});
