ef2d152dd1eb521c99409b4cc60315a0e13db9e6
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-01-17 11:35:10 +0530

    postCompleted

--- src/controllers/urlShortenController.js
+++ src/controllers/urlShortenController.js
@@ -1,32 +1,37 @@
 const shortid = require("shortid")
-const validUrl = require('valid-url')
+const validUrl = require('url-validation')
 //=> model importing
 const urlModel = require("../model/UrlModel");
 
 
-const baseUrl= "http:localhost:3000" 
-const createShortUrl=async function(req,res){
-try{
-    const {longUrl}=req.body
-    if(!validUrl.isUri(baseUrl)){
-        return res.status(401).send('Invalid base URL') 
-    }
-    const urlCode = shortid.generate();
-    if(validUrl.isUri(longUrl)){
-        let createUrl = await urlModel.findOne({longUrl})
-        if(createUrl)  {
-           res.send(findurl)
-        }else{
-           const shortUrl =baseUrl+ '/' + urlCode
-           createUrl=new urlModel({ urlCode , longUrl , shortUrl, data : new Date() })
-       }
-        res.status(200).send({status:true , data:createUrl})
+const baseUrl = "http:localhost:3000/"
+const createShortUrl = async function (req, res) {
+    try {
+        const { longUrl } = req.body
+        if(! longUrl) return res.status(400).send({status:false, message:"please provide long url"})
 
-  }else{
-       res.status(401).json('Invalid longUrl')
-   }
-} catch(err){
-      res.status(500).send({status:false , data:err.message})
+        if (! validUrl(longUrl)) return res.status(400).send({status:false, message:"please provide valid long url"})
+
+        const findUrl = await urlModel.findOne({ longUrl: longUrl }).select({urlCode:1,shortUrl:1,longUrl:1,_id:0})
+
+        if(findUrl) return res.status(200).send({status:true, data:findUrl})
+
+        let urlCode = shortid.generate();
+        
+        const findUrlCode= await urlModel.findOne({urlCode:urlCode})
+
+        if(findUrlCode) urlCode = urlCode + shortid.generate()
+
+        const shortUrl = baseUrl + urlCode
+
+        const crData = await urlModel.create({ urlCode: urlCode, longUrl: longUrl, shortUrl: shortUrl })
+
+        const result={longUrl: crData.longUrl, shortUrl: crData.shortUrl, urlCode: crData.urlCode}
+
+        return res.status(201).send({ status: true, data: result })
+
+    } catch (err) {
+        res.status(500).send({ status: false, data: err.message })
+    }
 }
-}
-module.exports={createShortUrl}
+module.exports = { createShortUrl }
