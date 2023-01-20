54d83ce66ed28dbafa6529b4746239f4be9d9212
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-01-20 18:13:36 +0530

    beulah completed project3

--- src/controllers/urlShortenController.js
+++ src/controllers/urlShortenController.js
@@ -3,17 +3,25 @@ const validUrl = require('url-validation')
 const urlModel = require("../model/UrlModel");
 const caching=require("../cacheFolder/cache")
 
-const baseUrl = "http:localhost:3000/"
+
 const createShortUrl = async function (req, res) {
-    
+  const baseUrl = "http:localhost:3000/"
   try {
-    const { longUrl } = req.body
+    let { longUrl } = req.body
+
     if (!longUrl) return res.status(400).send({ status: false, message: "please provide long url" })
+    longUrl=longUrl.trim();
 
     if (!validUrl(longUrl)) return res.status(400).send({ status: false, message: "please provide valid long url" })
 
+    if(longUrl.indexOf("https")==-1) longUrl=longUrl.replace("http","https")
+
     let cachedUrlData = await caching.GET_ASYNC(`${longUrl}`)
-    if(cachedUrlData) return res.status(200).send(cachedUrlData)
+    
+    if(cachedUrlData){
+      cachedUrlData=JSON.parse(cachedUrlData)
+      return res.status(200).send({status:true,data:cachedUrlData})
+    }
 
     const findUrl = await urlModel.findOne({ longUrl: longUrl }).select({ urlCode: 1, shortUrl: 1, longUrl: 1, _id: 0 })
 
@@ -22,11 +30,11 @@ const createShortUrl = async function (req, res) {
       return res.status(200).send({ status: true, data: findUrl })
     }
 
-    let urlCode = shortid.generate();
+    let urlCode = shortid.generate().toLowerCase();
 
     const findUrlCode = await urlModel.findOne({ urlCode: urlCode })
 
-    if (findUrlCode) urlCode = urlCode + shortid.generate()
+    if (findUrlCode) urlCode = urlCode + shortid.generate().toLowerCase()
 
     const shortUrl = baseUrl + urlCode
 
@@ -77,5 +85,3 @@ const getUrl = async function (req, res) {
   }
 }
 module.exports = { createShortUrl, getUrl }
-
-
